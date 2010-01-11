98e8236b658435c65e886037bc114457ca74405d
Clément Delafargue <divarvel@gmail.com>
2010-01-11 01:54:14 +0100

    Implémentation du contrôleur

--- src/main/java/com/ecnmelog/controller/StockageController.java
+++ src/main/java/com/ecnmelog/controller/StockageController.java
@@ -4,20 +4,75 @@ package com.ecnmelog.controller;
 
 import com.ecnmelog.model.AbstractStockage;
 import com.ecnmelog.model.AbstractAttente;
+import com.ecnmelog.model.Container;
+import com.ecnmelog.model.ContainerException;
+
+import java.util.ArrayList;
 
 /**
  * Partie du contrôleur dédiée à la communication avec le modèle
  */
-public class StockageController extends AbstractController {
-
-    public StockageController(AbstractStockage stock, AbstractAttente att) {
-        super(stock, att);
+public class StockageController {
+    protected AbstractStockage stock;
+    protected AbstractAttente att;
+    protected ArrayList<Integer> containerTypes;
+    
+    public StockageController(AbstractStockage stock, AbstractAttente att){
+        this.stock = stock;
+        this.att = att;
+        this.containerTypes = Container.getTypes();
     }
-
-    void control() {
-        //On notifie le modèle d'une action si le contrôle est bon
-        //--------------------------------------------------------
-        
-        
+    
+    /**
+     * Ajouter un container à la zone d'attente
+     * @param id L'identifiant du container à ajouter
+     * @param type Le type de container à ajouter
+     */
+    public void addContainer(int id, int type) {
+        if(this.containerTypes.contains(type)) {
+            try {
+                this.att.addContainer(new Container(id, type));
+            } catch (ContainerException e) {
+                // ToDo : Demander à la vue d'afficher un message d'erreur
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    
+    /**
+     * Stocker les containers en attente
+     */
+    public void traiterAttente() {
+        this.stock.traiterAttente();
+    }
+    
+    /**
+     * Supprimer un conteneur de la zone d'attente
+     * @param id L'identifiant du container à enlever
+     */
+    public void removeFromAttente(int id) {
+        this.att.removeContainerById(id);
+    }
+    
+    /**
+     * Supprimer un conteneur de la zone de stockage
+     * @param id L'identifiant du container à enlever
+     */
+    public void removeFromStockage(int id) {
+        this.stock.removeContainerById(id);
+    }
+    
+    /**
+     * Vider la zone d'attente
+     */
+    public void emptyAttente() {
+        this.att.empty();
+    }
+    
+    /**
+     * Vider la zone de stockage
+     */
+    public void emptyStockage() {
+        this.stock.empty();
     }
 }
